29901efc95238898238a6b6d187b9694590e8794
itsrepo <132600763+jejeeeeeee@users.noreply.github.com>
2025-01-04 17:56:42 +0100

    idle

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
-// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+bot.on("ready", () => {
+  bot.editStatus("idle");
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.on("error", (err) => {
+  console.error(err);
+});
+
+bot.connect();
